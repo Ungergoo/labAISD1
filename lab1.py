342dacb458e6f490f98dc93688129510db5a3389
Ungergoo <47298342+Ungergoo@users.noreply.github.com>
2023-05-05 14:59:49 +0400

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,15 +1,26 @@
-f = open('labtext1.txt', 'r')
+f = open('labtext.txt', 'r')
 massiv = f.read().split()
 massiv2 = []
 k = int(input())
-for i in range(len(massiv)):
-    if len(massiv[i]) == k:
-        massiv2.append(int(massiv[i]))
+clearm = []
+spisok = '0123456789'
+for l in massiv:
+    stroku = ''
+    for o in range(len(l)):
+        if (l[o]) in spisok:
+            stroku += l[o]
+    o+=1
+    clearm.append(stroku)
+for i in range(len(clearm)):
+    if len(clearm[i]) == k:
+        massiv2.append(int(clearm[i]))
 abc = ('ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять')
-minnum = (min(massiv2))
-print(len(massiv2))
-for j in str(minnum):
-    for k in range(10):
-        if j == str(k):
-            print(abc[k])
-        
+if massiv2 == []:
+    print("Таких элементов нет")
+else:
+    minnum = (len(massiv2))
+    print(min(massiv2))
+    for j in str(minnum):
+        for k in range(10):
+            if j == str(k):
+                print(abc[k])
